f8eb1655da584548cd9bf3a45b686d8142820670
Roman Mazeev <romanmazeev@icloud.com>
2021-12-21 22:24:03 +0300

    Reduce swift tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BinanceAPI",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_12)
     ],
     products: [
         .library(name: "BinanceAPI", targets: ["BinanceAPI"])
